d020bf87d4b16943c68bc2b61dda5ae8bef4c503
Benjamin Jönsson <benjamin.jonsson@geshdo.com>
2023-12-18 20:26:10 +0100

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import addRoutes from "./routes/add";
 import searchRoutes from "./routes/find";
@@ -7,6 +8,8 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/add", addRoutes);
 
