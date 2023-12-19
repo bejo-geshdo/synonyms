16834212137ba1b3ab230cf7241b5430d5cd000b
Benjamin Jönsson <benjamin.jonsson@geshdo.com>
2023-12-19 09:07:05 +0100

    Added try catch to find endpoint

--- backend/src/routes/add.ts
+++ backend/src/routes/add.ts
@@ -13,11 +13,16 @@ router.post("/", (req: Request, res: Response) => {
       .json({ error: "To few words to add needs at least 2" });
   }
 
+  //TODO add check if any word is an empty string or not string
+
+  //TODO add check if the same word appers twice
+
   try {
     synonyms.add(words);
     return res.status(201).json({ message: "Succesfully added synonym" });
   } catch (error) {
     console.error(error);
+    //TODO Security Don't return full error in production
     return res.status(500).json({ error: error });
   }
 });
--- backend/src/routes/find.ts
+++ backend/src/routes/find.ts
@@ -5,19 +5,25 @@ import { synonyms } from "../utils/dataStore";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  const word = req.query?.word;
+  try {
+    const word = req.query?.word;
 
-  if (typeof word !== "string") {
-    return res.status(400).json({ error: "Missing word in query" });
+    if (typeof word !== "string") {
+      return res.status(400).json({ error: "Missing word in query" });
+    }
+
+    const words = synonyms.findAllSynonyms(word);
+
+    if (words.length < 2) {
+      return res.status(404).json({ error: `No synonyms found for ${word}` });
+    }
+
+    return res.status(200).json({ message: words });
+  } catch (error) {
+    console.error(error);
+    //TODO Security Don't return full error in production
+    return res.status(500).json({ error: error });
   }
-
-  const words = synonyms.findAllSynonyms(word);
-
-  if (words.length < 2) {
-    return res.status(404).json({ error: `No synonyms found for ${word}` });
-  }
-
-  return res.status(200).json({ message: words });
 });
 
 export default router;
