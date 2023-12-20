7bc85e978ae205813f37e60d2b344952e1aef87f
Benjamin Jönsson <benjamin.jonsson@geshdo.com>
2023-12-20 13:41:16 +0100

    added VPC to TF

---
+++ infra/vpc.tf
@@ -0,0 +1,56 @@
+# --- VPC ---
+
+#Get's a list of azs from AWS
+data "aws_availability_zones" "available" { state = "available" }
+
+# Here you can select how man azs you want
+# We only use 1 for now
+#TODO findout why we need 2 :(
+locals {
+  azs_count = 2
+  azs_names = data.aws_availability_zones.available.names
+}
+
+//Creates the VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_subnet" "public" {
+  count                   = local.azs_count
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = local.azs_names[count.index]
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 10 + count.index)
+  map_public_ip_on_launch = true
+}
+
+# --- Internet Gateway ---
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+#TODO findout why they use this
+resource "aws_eip" "main" {
+  count      = local.azs_count
+  depends_on = [aws_internet_gateway.main]
+}
+
+# --- Public Route Table ---
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = local.azs_count
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
