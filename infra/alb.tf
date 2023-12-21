ae47b27cdf1d43e30b42cad9597b7f72f41ddf31
Benjamin Jönsson <benjamin.jonsson@geshdo.com>
2023-12-21 10:26:21 +0100

    Made the https only

--- infra/alb.tf
+++ infra/alb.tf
@@ -37,12 +37,12 @@ resource "aws_lb_target_group" "app" {
   port        = 80
   target_type = "ip"
 
-  #TODO add an other path in API for helth checks
+  #TODO add an other path in API that returns 200 for helth checks
   health_check {
     enabled             = true
     path                = "/find"
     port                = 80
-    matcher             = 200
+    matcher             = 400
     interval            = 10
     timeout             = 5
     healthy_threshold   = 2
@@ -51,17 +51,6 @@ resource "aws_lb_target_group" "app" {
 }
 
 #How we access the lb
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.main.id
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app.id
-  }
-}
-
 resource "aws_lb_listener" "https" {
   depends_on        = [time_sleep.api_cert_dns]
   load_balancer_arn = aws_lb.main.id
@@ -98,6 +87,8 @@ resource "aws_route53_record" "api_cert_dns" {
   ttl             = 300
 }
 
+#Added this to give the cert time to validate before adding it to ALB
+#TODO Find a better way to check if the cert has validate, if it's not validate terraform will get an error
 resource "time_sleep" "api_cert_dns" {
   depends_on = [aws_route53_record.api_cert_dns]
 
